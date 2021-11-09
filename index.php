040da28e792775e611e8fd61e1cd7a0991867f57
OlgMax <36724760+OlgMax@users.noreply.github.com>
2021-11-09 19:04:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,36 +2,28 @@
 require_once 'vendor/autoload.php';
 use \Core\Router;
 use Lib\DB\Connector;
+use Lib\DB\Select;
+use Lib\DB\Where;
+
 /*$router = new Router();
 $router->run();*/
 $connector = new Connector();
 $conn = $connector->getConnect();
-$stmt = $conn->prepare("INSERT INTO posts(subject, detail, author_id) VALUES('title post', 'description post', 5)");
-$stmt = $conn->prepare("INSERT INTO users(user_name, surname, age) VALUES('Ivan', 'Ivanov', 25)");
-$stmt = $conn->prepare("INSERT INTO contacts(email, phone, city) VALUES('some@mail.com', '123456', 'Dnepr')");
-$stmt = $conn->prepare("UPDATE posts SET subject='some subject' WHERE author_id=2");
-$stmt = $conn->prepare("UPDATE users SET user_name='Bob' WHERE id=1");
-$stmt = $conn->prepare("DELETE FROM contacts WHERE id = 3");
-$stmt = $conn->prepare("DELETE FROM posts WHERE detail = 'description post'");
-$stmt->execute();
 
-try
-{
-    $stmt = "SELECT email, phone, city FROM contacts";
-    $res = $conn->query($stmt);
-    if ($res->rowCount() > 0) {
-        $row = $res->fetch();
-        echo $row['email'];
-        echo $row['phone'];
-        echo $row['city'];
-        }
-}
-catch (PDOException $e)
-{
-    die("ERROR: Could not able to execute $stmt. ".$e->getMessage());
-}
+$select = new Select();
+$select->setTableNames('contacts');
+$select->setLimited(1);
+$select->setFieldNames('city');
+$sglString = $select->getSqlString();
 
-
+$where = new Where();
+$where->setFieldNames('city');
+$where->setOperator('OR');
+$where->setParam('Dnepr');
+$sglStringW = $where->getSqlWhereString();
+$exec = $conn->prepare($sglString.$sglStringW);
+$exec->execute;
+print_r($exec);
 
 
 
