659418e1457eea3709cd4f28fdb5378273ab1c0c
OlgMax <36724760+OlgMax@users.noreply.github.com>
2021-11-19 20:03:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php
 require_once 'vendor/autoload.php';
 use Core\Router;
-use Lib\DB\Common\Connector;
-use Lib\DB\Select;
-use Lib\DB\Where;
-
 
 $router = new Router();
 $router->run();
-$connector = new Connector();
-$conn = $connector->getConnect();
